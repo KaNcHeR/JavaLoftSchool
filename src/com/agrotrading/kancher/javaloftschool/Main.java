b6032097bc0d704c73b09eeb3f2ef2cf9f1f1bfc
KaNcHeR <tmnkach@mail.ru>
2016-01-17 01:45:49 +0500

    This is warm-up

--- src/com/agrotrading/kancher/javaloftschool/Main.java
+++ src/com/agrotrading/kancher/javaloftschool/Main.java
@@ -1,8 +1,44 @@
 package com.agrotrading.kancher.javaloftschool;
 
+import java.io.File;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        byte word [] = new byte[256];
+        int counterChar;
+
+        while (true) {
+
+            System.out.print(StringApp.enterCommandAlert);
+
+            try {
+                counterChar = System.in.read(word);
+            } catch (Exception ex) {
+                System.out.println(ex.toString());
+                break;
+            }
+
+            String strWord = new String(word, 0, counterChar).trim();
+
+            if (StringApp.exitCommand.equalsIgnoreCase(strWord)) {
+                System.out.print(StringApp.goodbye);
+                break;
+            } else {
+
+                File myFolder = new File(strWord);
+
+                File[] files = myFolder.listFiles();
+
+                try {
+                    for (File file : files) {
+                        System.out.println(file.getName());
+                    }
+                } catch (NullPointerException e) {
+                    System.out.println(StringApp.errorNullItem);
+                }
+            }
+        }
     }
 }
---
+++ src/com/agrotrading/kancher/javaloftschool/StringApp.java
@@ -0,0 +1,10 @@
+package com.agrotrading.kancher.javaloftschool;
+
+public class StringApp {
+
+    public static final String enterCommandAlert = "Введите команду: ";
+    public static final String exitCommand = "exit";
+    public static final String errorNullItem = "Каталога не сущестует, либо отсутствуют текстовые файлы\n";
+    public static final String goodbye = "Всего доброго!";
+
+}
